367fb879c24563146230c716dedeed78a54d29da
Kristaps Vītoliņš <kristaps.vitolins@gmail.com>
2018-04-07 20:41:40 +0300

    init

---
+++ Job/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Job
+{
+    class Program
+    {
+        static int Ackermann(int m, int n)
+        {
+            int answer;
+
+            if (m == 0)
+            {
+                answer = n + 1;
+            }
+            else if (n == 0)
+            {
+                answer = Ackermann(m - 1, 1);
+            }
+            else
+            {
+                answer = Ackermann(m - 1, Ackermann(m, m - 1));
+            }
+            return answer;
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    Console.WriteLine($"Ackermann {i}, {j}, {Ackermann(i, j)}");
+                }
+            }
+        }
+    }
+}
